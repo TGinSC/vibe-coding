76b3598d8eb9c05e5ff4edd45a13766d6e6d2139
pyke <pykelysia0206@163.com>
2025-08-03 22:39:40 +0800

    add test and example in packsge data

--- back_end/main.go
+++ back_end/main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"contribution/data"
 	"contribution/database"
 )
 
 func main() {
 	database.Open("data.db")
-	database.StoreExampleData()
-	database.GetExampleData()
-	database.UpdataExampleData()
-	database.GetExampleData()
-	database.DeleteExampleData()
+	data.StoreExampleData()
+	data.GetExampleData()
+	data.UpdataExampleData()
+	data.GetExampleData()
+	data.DeleteExampleData()
 	// server := gin.Default()
 
 	// server.Run()
